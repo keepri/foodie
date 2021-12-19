8af251b9d5819602a17084e8b198f76960184bed
Ciprian-Liviu Pîțul <kipri@192-168-0-186.rdsnet.ro>
2021-12-19 13:01:44 +0200

    configured eslint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,9 @@
-import * as functions from "firebase-functions";
+import { https, logger } from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = https.onRequest((req, res) => {
+	logger.info('Hello logs!', { structuredData: true });
+	res.status(200).json({ msg: 'Hello from Firebase!' });
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,27 @@ import { Provider } from 'react-redux';
 
 import { store } from '#redux/store';
 
-const Foodie = ({ Component, pageProps }: AppProps) => (
-	<Provider store={store}>
-		<Component {...pageProps} />
-	</Provider>
-);
+import { initializeApp } from 'firebase/app';
+import React from 'react';
+
+const Foodie = ({ Component, pageProps }: AppProps) => {
+	const firebaseConfig = {
+		apiKey: 'AIzaSyDh8jzT_aqjFSMXblgfUvaONVpo8H0GYv4',
+		authDomain: 'foodie-55977.firebaseapp.com',
+		projectId: 'foodie-55977',
+		storageBucket: 'foodie-55977.appspot.com',
+		messagingSenderId: '155703867126',
+		appId: '1:155703867126:web:ca5e81e97ac95dc9565ed7',
+		measurementId: 'G-F3MCGXRK3V',
+	};
+
+	initializeApp(firebaseConfig);
+
+	return (
+		<Provider store={store}>
+			<Component {...pageProps} />
+		</Provider>
+	);
+};
 
 export default Foodie;
