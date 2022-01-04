3dbb722e6e1e09ebcbf89e6e7c22e5edf40d6b40
Ciprian-Liviu Pîțul <kipri@192-168-0-186.rdsnet.ro>
2022-01-04 18:26:42 +0200

    m

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import type { NextPage } from 'next';
-import Button from '#components/Buttons/Button';
-import Link from '#components/Buttons/Link';
 
 interface Props {}
 
 const HomePage: NextPage<Props> = ({}) => {
-	return (
-		<div>
-			<Button secondary>Hello</Button>
-			<Link href='#'>Hello Link</Link>
-		</div>
-	);
+	return <div></div>;
 };
 
 export default HomePage;
