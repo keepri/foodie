4d494307042e7506440cefe5acc291fb73326574
Ciprian-Liviu Pîțul <kipri@192-168-0-196.rdsnet.ro>
2022-01-07 20:57:46 +0200

    moved temp navigation to navbar component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,11 +1,41 @@
+import Link from '#components/Buttons/Link';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { URLS } from 'utils/misc';
+import { ReduxState } from '#declarations/types/Redux';
+import Button from '#components/Buttons/Button';
+import { useAuthActions } from '#redux/actions';
 
 interface Props {}
 
 const Navbar: React.FC<Props> = ({}) => {
+	const {
+		auth: { isLogged },
+	} = useSelector(({ auth }: ReduxState) => ({ auth }));
+	const { logoutUserAuth } = useAuthActions();
+
 	return (
 		<nav>
-			<ul></ul>
+			<ul>
+				<Link button secondary href={URLS.CART}>
+					CART
+				</Link>
+				{isLogged && (
+					<Button secondary onMouseUp={() => logoutUserAuth()}>
+						LOGOUT
+					</Button>
+				)}
+				{!isLogged && (
+					<>
+						<Link button secondary href={URLS.LOGIN}>
+							LOGIN
+						</Link>
+						<Link button secondary href={URLS.REGISTER}>
+							REGISTER
+						</Link>
+					</>
+				)}
+			</ul>
 		</nav>
 	);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,19 @@
 import React from 'react';
 import type { NextPage } from 'next';
 
-import Button from '#components/Buttons/Button';
-import { useAuthActions } from '#redux/actions';
 import { useSelector } from 'react-redux';
 import { ReduxState } from '#declarations/types/Redux';
-import Link from '#components/Buttons/Link';
-import { URLS } from 'utils/misc';
 
 interface Props {}
 
 const Index: NextPage<Props> = ({}) => {
 	const {
-		auth: { isLogged, user },
+		auth: { user },
 	} = useSelector(({ auth }: ReduxState) => ({ auth }));
-	const { logoutUserAuth } = useAuthActions();
 
 	return (
 		<main>
 			<h1>Hello{`, ${user.name}`}</h1>
-
-			<Link button secondary href={URLS.CART}>
-				CART
-			</Link>
-
-			{!isLogged && (
-				<Link button secondary href={URLS.LOGIN}>
-					Login
-				</Link>
-			)}
-			{!isLogged && (
-				<Link button secondary href={URLS.REGISTER}>
-					Register
-				</Link>
-			)}
-			{isLogged && (
-				<Button primary onMouseUp={logoutUserAuth}>
-					Logout
-				</Button>
-			)}
 		</main>
 	);
 };
