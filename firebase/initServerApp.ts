e5634d2a42073c5b69c5fffe3020f7498c5b1b87
Ciprian-Liviu Pîțul <kipri@192-168-0-194.rdsnet.ro>
2022-03-25 14:18:31 +0200

    private key

--- firebase/initServerApp.ts
+++ firebase/initServerApp.ts
@@ -14,7 +14,7 @@ if (!admin.apps.length) {
 	admin.initializeApp({
 		credential: admin.credential.cert({
 			projectId: process.env.FIREBASE_PROJECT_ID,
-			privateKey: JSON.parse(process.env.FIREBASE_PRIVATE_KEY ?? ''),
+			privateKey: process.env.FIREBASE_PRIVATE_KEY,
 			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 		}),
 		...firebaseConfig,
