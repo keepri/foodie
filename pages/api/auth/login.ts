ed86ab5e1f1127cd01ab3b96e719e9f6f3955a88
KIPRI <hi@kipri.dev>
2023-03-15 08:54:13 +0200

    ran linter with --fix on code base

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -13,38 +13,38 @@ import { destroyCookie, setCookie } from 'nookies';
 // import nodemailer from 'nodemailer';
 
 export default async (req: NextApiRequest, res: NextApiResponse<LoginReturnType>) => {
-	switch (req.method?.toUpperCase()) {
-		case REQUEST_METHODS.POST: {
-			try {
-				const { token } = req.body as ClientLoginFields;
-				if (!token) return res.status(401).json({ message: MESSAGES.UNAUTHORIZED_NO_TOKEN });
+    switch (req.method?.toUpperCase()) {
+        case REQUEST_METHODS.POST: {
+            try {
+                const { token } = req.body as ClientLoginFields;
+                if (!token) return res.status(401).json({ message: MESSAGES.UNAUTHORIZED_NO_TOKEN });
 
-				// const sessionCookie = await auth.createSessionCookie(token, {expiresIn: 604800000}) // 604800000 = one week in milliseconds
+                // const sessionCookie = await auth.createSessionCookie(token, {expiresIn: 604800000}) // 604800000 = one week in milliseconds
 
-				const checkRevoked = true;
-				const { uid } = await auth.verifyIdToken(token, checkRevoked);
-				const userDoc = await firestore.collection(COLLECTIONS.CLIENTS).doc(uid).get();
+                const checkRevoked = true;
+                const { uid } = await auth.verifyIdToken(token, checkRevoked);
+                const userDoc = await firestore.collection(COLLECTIONS.CLIENTS).doc(uid).get();
 
-				if (!userDoc.exists) {
-					await auth.revokeRefreshTokens(uid);
-					destroyCookie({ res }, COOKIE_NAMES.TOKEN);
-					return res.status(404).json({ message: MESSAGES.NOT_FOUND });
-				}
+                if (!userDoc.exists) {
+                    await auth.revokeRefreshTokens(uid);
+                    destroyCookie({ res }, COOKIE_NAMES.TOKEN);
+                    return res.status(404).json({ message: MESSAGES.NOT_FOUND });
+                }
 
-				const user = userDoc.data() as ClientSchema;
-				const tokenCookie = 'Bearer ' + token;
+                const user = userDoc.data() as ClientSchema;
+                const tokenCookie = 'Bearer ' + token;
 
-				setCookie({ res }, COOKIE_NAMES.TOKEN, tokenCookie, defaultCookieOptions);
+                setCookie({ res }, COOKIE_NAMES.TOKEN, tokenCookie, defaultCookieOptions);
 
-				return res.status(200).json({ user, message: MESSAGES.LOGIN_ACCOUNT_SUCCESS });
-			} catch (error) {
-				handleError(error, res);
-			}
+                return res.status(200).json({ user, message: MESSAGES.LOGIN_ACCOUNT_SUCCESS });
+            } catch (error) {
+                handleError(error, res);
+            }
 
-			break;
-		}
+            break;
+        }
 
-		default:
-			return res.status(405).json({ message: `Method "${req.method}" not allowed!` });
-	}
+        default:
+            return res.status(405).json({ message: `Method "${req.method}" not allowed!` });
+    }
 };
